d08296614667d29de13b2f9d56d302c4fd96ac50
goyalsushant1 <sushant.goyal@tothenew.com>
2019-02-20 09:23:51 +0530

    exercise 7 updated

--- exercise7.php
+++ exercise7.php
@@ -1,11 +1,14 @@
 <?php
 $array = [1,2,3,4,5,6,7];
-foreach ($array as $values) {
+for($i=0;$i<sizeof($array);$i++) {
     # code...
-    if($values==4 || $values==5) {
+    if($array[$i]==4 || $array[$i]==5) {
         continue;
     }
+    if($i==(sizeof($array)-1)){
+        echo $array[$i];
+    }
     else
-        echo $values . ' ';
+        echo $array[$i] . ', ';
 }
 ?>
